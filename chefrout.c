e475ce31c643da565daf4dcb4425fcdc5e02fd50
Arvind Ramadurai <arvindramadurai7@gmail.com>
2017-05-07 23:38:19 +0530

    Add files via upload

---
+++ chefrout.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<string.h>
+void sort(char a[])
+{
+	int i,j,n;
+	n=strlen(a);
+	char temp;
+	for(i=0;i<n;i++)
+		for(j=0;j<n-1;j++)
+			if(a[j]>a[j+1])
+			{
+				temp=a[j+1];
+				a[j+1]=a[j];
+				a[j]=temp;
+			}
+}
+int main()
+{
+	int t,i,j,ans[100];
+	char a[100][100],b[100][100];
+	scanf("%d",&t);
+	for(i=0;i<t;i++)
+		scanf("%s",a[i]);
+	for(i=0;i<t;i++)
+	{
+		strcpy(b[i],a[i]);
+		sort(a[i]);
+	}
+	
+	for(i=0;i<t;i++)
+	{
+		if(strcmp(a[i],b[i])==0)
+			ans[i]=1;
+		else if(a[i][0]=='E')
+			ans[i]=0;
+		else
+			ans[i]=0;
+	}
+	for(i=0;i<t;i++)
+	{
+		if(ans[i]==1)
+			printf("yes\n");	
+		else
+			printf("no\n");
+	}
+	return 0;
+}
+		
